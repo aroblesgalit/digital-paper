780783f1b6e5d558dd34fadf90d19bd8ffc8f723
Alvin Galit <aroblesgalit@gmail.com>
2022-03-17 21:55:50 -0600

    Created connection to DB

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 5000
 
@@ -9,6 +10,18 @@ const PORT = process.env.PORT || 5000
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
+
+// Connect to DB
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  () => {
+    console.log('Connected to DB!')
+  }
+)
 
 // Start API server
 app.listen(PORT, function () {
