445672095aea2e93ab695aff17fae026b5a69974
Alvin Galit <aroblesgalit@gmail.com>
2022-03-18 23:32:59 -0600

    Created users controller with a method for registering a user

---
+++ controllers/usersController.js
@@ -0,0 +1,17 @@
+const db = require('../models')
+
+module.exports = {
+  registerUser: function (req, res) {
+    db.User.create({
+      username: req.body.username,
+      password: req.body.password
+    })
+      .then(dbUser =>
+        res.json({
+          id: dbUser._id,
+          username: dbUser.username
+        })
+      )
+      .catch(err => res.status(401).json(err))
+  }
+}
