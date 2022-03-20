7aeea4b3b9b6e5b9a94b5873160aea0717446ba5
Alvin Galit <aroblesgalit@gmail.com>
2022-03-19 19:54:14 -0600

    Created Nav component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <Nav />
   </div>
-  <router-view />
 </template>
+
+<script>
+import Nav from './components/Nav'
+
+export default {
+  components: { Nav }
+}
+</script>
 
 <style></style>
