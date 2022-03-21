0c248dc5ae0457903f5c7e7716fe9c531f72e237
Alvin Galit <aroblesgalit@gmail.com>
2022-03-20 20:13:46 -0600

    Added form to Register view

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,5 +1,34 @@
 <template>
-  <h1>Register</h1>
+  <div class="d-flex flex-column align-items-center">
+    <div class="card mw-100 mb-3">
+      <div class="card-body">
+        <h1 class="card-title h3 mb-3 fw-normal text-center">Register</h1>
+        <form class="d-flex flex-column">
+          <div class="mb-3">
+            <label for="username" class="form-label">Username</label>
+            <input id="username" type="text" class="form-control" required />
+          </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input
+              id="password"
+              type="password"
+              class="form-control"
+              required
+            />
+          </div>
+          <div class="mb-3">
+            <label for="confirm" class="form-label">Confirm password</label>
+            <input id="confirm" type="password" class="form-control" required />
+          </div>
+          <button type="submit" class="btn btn-primary align-self-end">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+    <p>Already have an account? Log in here</p>
+  </div>
 </template>
 
 <script>
@@ -8,8 +37,8 @@ export default {
 }
 </script>
 
-<style>
-body {
-  min-height: 75rem;
+<style scoped>
+.card {
+  width: 400px;
 }
 </style>
