d07fc31587fd0030fb656ae43cddd772685a0458
Alvin Galit <aroblesgalit@gmail.com>
2022-03-21 23:07:59 -0600

    Created action in store for registering a user

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,28 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex'
+import createdPersistedState from 'vuex-persistedstate'
+import axios from 'axios'
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+  plugins: [createdPersistedState()],
+  state: {
+    registration: ''
+  },
+  mutations: {
+    SET_REGS: (state, payload) => (state.registration = payload)
+  },
+  actions: {
+    async registerUser ({ commit }, data) {
+      try {
+        await axios.post('http://localhost:5000/api/user/register', data)
+        commit('SET_REGS', 'Congratulations! You have successfully registered.')
+      } catch (err) {
+        console.error(err)
+        commit(
+          'SET_REGS',
+          'Oh no, something went wrong! Please try again later.'
+        )
+      }
+    }
+  },
+  modules: {}
+})
