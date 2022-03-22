eec99cab84b7c0abdf3630918ed0a9e1b0e157b3
Alvin Galit <aroblesgalit@gmail.com>
2022-03-21 23:01:20 -0600

    Created data object with username, password, and confirm

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -6,11 +6,18 @@
         <form class="d-flex flex-column">
           <div class="mb-3">
             <label for="username" class="form-label">Username</label>
-            <input id="username" type="text" class="form-control" required />
+            <input
+              v-model="username"
+              id="username"
+              type="text"
+              class="form-control"
+              required
+            />
           </div>
           <div class="mb-3">
             <label for="password" class="form-label">Password</label>
             <input
+              v-model="password"
               id="password"
               type="password"
               class="form-control"
@@ -19,7 +26,13 @@
           </div>
           <div class="mb-3">
             <label for="confirm" class="form-label">Confirm password</label>
-            <input id="confirm" type="password" class="form-control" required />
+            <input
+              v-model="confirm"
+              id="confirm"
+              type="password"
+              class="form-control"
+              required
+            />
           </div>
           <button type="submit" class="btn btn-primary align-self-end">
             Submit
@@ -33,7 +46,14 @@
 
 <script>
 export default {
-  name: 'Register'
+  name: 'Register',
+  data () {
+    return {
+      username: '',
+      password: '',
+      confirm: ''
+    }
+  }
 }
 </script>
 
