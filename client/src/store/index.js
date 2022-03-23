18ac9c559ced73e39a7c2ff5c86b06fe519c4880
Alvin Galit <aroblesgalit@gmail.com>
2022-03-22 21:46:21 -0600

    Created action for logging in user

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,10 +5,12 @@ import axios from 'axios'
 export default createStore({
   plugins: [createdPersistedState()],
   state: {
-    registration: null
+    registration: null,
+    user: null
   },
   mutations: {
-    SET_REGS: (state, payload) => (state.registration = payload)
+    SET_REGS: (state, payload) => (state.registration = payload),
+    SET_USER: (state, payload) => (state.user = payload)
   },
   actions: {
     async registerUser ({ commit }, data) {
@@ -21,6 +23,19 @@ export default createStore({
           'SET_REGS',
           'Oh no, something went wrong! Please try again later.'
         )
+      }
+    },
+    async loginUser ({ commit }, data) {
+      try {
+        const response = await axios.post(
+          'http://localhost:5000/api/user/login',
+          data
+        )
+        console.log(response.data)
+        commit('SET_USER', response.data)
+      } catch (err) {
+        console.error(err)
+        commit('SET_USER', {})
       }
     }
   },
