60faa917926c533e66f49d18e2feb0d7b92c7e05
Alvin Galit <aroblesgalit@gmail.com>
2022-03-24 23:15:16 -0600

    Added a navigation guard to routers

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import Register from '../views/Register.vue'
 import Login from '../views/Login.vue'
 import Dashboard from '../views/Dashboard.vue'
+import store from '../store'
 
 const routes = [
   {
@@ -15,7 +16,7 @@ const routes = [
     name: 'Register',
     component: Register,
     meta: {
-      authRequired: true
+      authRequired: false
     }
   },
   {
@@ -39,6 +40,25 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = store.getters['user/isAuthenticated']
+  if ('authRequired' in to.meta && to.meta.authRequired) {
+    if (isAuthenticated) {
+      next()
+    } else {
+      next('/login')
+    }
+  } else if ('authRequired' in to.meta && !to.meta.authRequired) {
+    if (isAuthenticated) {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
