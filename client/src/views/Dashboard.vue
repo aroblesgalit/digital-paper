3f1e1ad856fe9d4866c0923ba164d3543430bffe
Alvin Galit <aroblesgalit@gmail.com>
2022-03-24 22:48:28 -0600

    Created basic template for Dashboard view

---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>
+    Protected Page &mdash; If you're viewing this, that means you are logged in.
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard'
+}
+</script>
+
+<style scoped></style>
