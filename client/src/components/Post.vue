33fdc68bf07f233539308680a89f9dd4c50e42d2
Alvin Galit <aroblesgalit@gmail.com>
2022-03-25 23:58:01 -0600

    Created a bootstrap card component for Post with header and body

--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -1,5 +1,34 @@
 <template>
-  <h2>Post</h2>
+  <div class="card">
+    <div class="card-header d-flex">
+      <div class="me-3">
+        <img src="https://via.placeholder.com/60" alt="" />
+      </div>
+      <div class="d-flex flex-column justify-content-center">
+        <p class="m-0">Persons name</p>
+        <span>1d</span>
+      </div>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Post title</h5>
+      <p class="card-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non quam
+        quis nibh elementum lacinia. Curabitur maximus dapibus risus sed porta.
+        Quisque enim elit, cursus a elit a, elementum gravida leo. Donec vitae
+        massa et tellus tempus pretium. Aliquam volutpat porttitor magna in
+        facilisis. Sed vel placerat leo, ac vehicula nulla. Duis odio leo,
+        consequat at viverra nec, iaculis eu odio. Duis facilisis, dolor vel
+        consequat maximus, dolor lacus tempus neque, a malesuada felis mi ut
+        lacus. Integer sed augue ut nulla rutrum pretium quis vel purus. Etiam
+        quam odio, gravida non quam et, venenatis consequat nunc. Sed pretium
+        placerat ullamcorper. Suspendisse vestibulum imperdiet ligula et
+        faucibus. In in blandit arcu, et pellentesque tortor.
+      </p>
+    </div>
+    <div class="card-footer">
+      <a href="#" class="card-link">Read more</a>
+    </div>
+  </div>
 </template>
 
 <script>
