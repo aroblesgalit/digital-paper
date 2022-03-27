1a6420c38954a43267f96fef80a4ebaaf1cee0cc
Alvin Galit <aroblesgalit@gmail.com>
2022-03-27 00:34:10 -0600

    Added modal for creating a post

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -3,10 +3,53 @@
     <div class="card">
       <div class="card-body d-flex justify-content-between">
         <h2>Posts</h2>
-        <button class="btn btn-primary">
+        <button
+          type="button"
+          class="btn btn-primary"
+          data-bs-toggle="modal"
+          data-bs-target="#postModal"
+        >
           Create
         </button>
+        <!-- Modal -->
+        <div
+          class="modal fade"
+          id="postModal"
+          tabindex="-1"
+          aria-labelledby="createPost"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="createPost">Modal title</h5>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body">
+                Woohoo, you're reading this text in a modal!
+              </div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" class="btn btn-primary">
+                  Save changes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
       <div class="card-body">
         <table class="table table-hover">
           <thead>
