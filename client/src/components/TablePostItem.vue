41ecb71a2d541b608aea34ea3b80e5a5552b2a23
Alvin Galit <aroblesgalit@gmail.com>
2022-03-29 11:54:46 -0600

    Created component for a post item on the table

---
+++ client/src/components/TablePostItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <tr>
+    <td scope="row">{{ post.title }}</td>
+    <td>{{ formattedDate }}</td>
+    <td>{{ post.likes }}</td>
+    <td>{{ numOfComments }}</td>
+    <td>
+      <span class="bi bi-pencil me-3"></span>
+      <span class="bi bi-trash3"></span>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'TablePostItem',
+  props: {
+    post: Object
+  },
+  computed: {
+    formattedDate () {
+      let date = new Date(this.post.createdAt)
+      let month = date.getMonth()
+      let day = date.getDate()
+      let year = date.getFullYear()
+
+      return month + '/' + day + '/' + year
+    },
+    numOfComments () {
+      return this.post.comments.length
+    }
+  }
+}
+</script>
+
+<style></style>
