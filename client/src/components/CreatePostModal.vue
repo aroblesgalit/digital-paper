50dac91ecc72ab5c604be871b19be96c99805f9b
Alvin Galit <aroblesgalit@gmail.com>
2022-03-29 17:29:56 -0600

    Created component for the modal to create a post

---
+++ client/src/components/CreatePostModal.vue
@@ -0,0 +1,97 @@
+<template>
+  <div
+    class="modal fade"
+    id="postModal"
+    tabindex="-1"
+    aria-labelledby="createPost"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title text-center" id="createPost">
+            Create a post
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <form class="row g-3">
+            <div class="col-12">
+              <label for="inputTitle" class="form-label">Title</label>
+              <input
+                type="text"
+                class="form-control"
+                id="inputTitle"
+                placeholder="Title"
+              />
+            </div>
+            <div class="col-md-6">
+              <label for="inputCategory" class="form-label">Category</label>
+              <select id="inputCategory" class="form-select">
+                <option selected>Choose...</option>
+                <option>...</option>
+              </select>
+            </div>
+            <div class="col-md-6">
+              <label for="inputImage" class="form-label">Image</label>
+              <input type="text" class="form-control" id="inputImage" />
+            </div>
+            <div class="col-12">
+              <label for="inputBody" class="form-label">Body</label>
+              <input
+                type="text"
+                class="form-control"
+                id="inputBody"
+                placeholder="Body"
+              />
+            </div>
+            <div class="col-12">
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  id="publicCheck"
+                />
+                <label class="form-check-label" for="publicCheck">
+                  Make public
+                </label>
+              </div>
+            </div>
+            <div class="col-12 d-flex justify-content-end">
+              <button
+                type="button"
+                class="btn btn-outline-secondary me-3"
+                data-bs-dismiss="modal"
+              >
+                Cancel
+              </button>
+              <button type="submit" class="btn btn-primary">
+                Create
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreatePostModal'
+}
+</script>
+
+<style scoped>
+.modal form label {
+  font-size: 12px;
+}
+.modal-header h5 {
+  width: 100%;
+}
+</style>
--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -12,88 +12,7 @@
           Create
         </button>
         <!-- Modal -->
-        <div
-          class="modal fade"
-          id="postModal"
-          tabindex="-1"
-          aria-labelledby="createPost"
-          aria-hidden="true"
-        >
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title text-center" id="createPost">
-                  Create a post
-                </h5>
-                <button
-                  type="button"
-                  class="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                ></button>
-              </div>
-              <div class="modal-body">
-                <form class="row g-3">
-                  <div class="col-12">
-                    <label for="inputTitle" class="form-label">Title</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="inputTitle"
-                      placeholder="Title"
-                    />
-                  </div>
-                  <div class="col-md-6">
-                    <label for="inputCategory" class="form-label"
-                      >Category</label
-                    >
-                    <select id="inputCategory" class="form-select">
-                      <option selected>Choose...</option>
-                      <option>...</option>
-                    </select>
-                  </div>
-                  <div class="col-md-6">
-                    <label for="inputImage" class="form-label">Image</label>
-                    <input type="text" class="form-control" id="inputImage" />
-                  </div>
-                  <div class="col-12">
-                    <label for="inputBody" class="form-label">Body</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="inputBody"
-                      placeholder="Body"
-                    />
-                  </div>
-                  <div class="col-12">
-                    <div class="form-check">
-                      <input
-                        class="form-check-input"
-                        type="checkbox"
-                        id="publicCheck"
-                      />
-                      <label class="form-check-label" for="publicCheck">
-                        Make public
-                      </label>
-                    </div>
-                  </div>
-                  <div class="col-12 d-flex justify-content-end">
-                    <button
-                      type="button"
-                      class="btn btn-outline-secondary me-3"
-                      data-bs-dismiss="modal"
-                    >
-                      Cancel
-                    </button>
-                    <button type="submit" class="btn btn-primary">
-                      Create
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
+        <CreatePostModal />
       </div>
 
       <div class="card-body">
@@ -105,13 +24,15 @@
 
 <script>
 import TablePosts from '../components/TablePosts.vue'
+import CreatePostModal from '../components/CreatePostModal.vue'
 import { createNamespacedHelpers } from 'vuex'
 const postModule = createNamespacedHelpers('post')
 
 export default {
   name: 'Dashboard',
   components: {
-    TablePosts
+    TablePosts,
+    CreatePostModal
   },
   methods: {
     ...postModule.mapActions(['getUserPosts'])
@@ -135,11 +56,5 @@ export default {
 .card {
   width: 1000px;
   max-width: 90vw;
-}
-.modal form label {
-  font-size: 12px;
-}
-.modal-header h5 {
-  width: 100%;
 }
 </style>
