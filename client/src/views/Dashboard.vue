8efbd17a788978cd8119adc950a4bb1e4e6e71ab
Alvin Galit <aroblesgalit@gmail.com>
2022-03-29 11:58:49 -0600

    Imported TablePosts component into dashboard view

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -97,77 +97,20 @@
       </div>
 
       <div class="card-body">
-        <table class="table table-hover">
-          <thead>
-            <tr>
-              <th scope="col">Title</th>
-              <th scope="col">Date created</th>
-              <th scope="col">Likes</th>
-              <th scope="col">Comments</th>
-              <th class="col">&nbsp;</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td scope="row">Dreams</td>
-              <td>03/18/22</td>
-              <td>24</td>
-              <td>7</td>
-              <td>
-                <span class="bi bi-pencil me-3"></span>
-                <span class="bi bi-trash3"></span>
-              </td>
-            </tr>
-            <tr>
-              <td scope="row">Dreams</td>
-              <td>03/18/22</td>
-              <td>24</td>
-              <td>7</td>
-              <td>
-                <span class="bi bi-pencil me-3"></span>
-                <span class="bi bi-trash3"></span>
-              </td>
-            </tr>
-            <tr>
-              <td scope="row">Dreams</td>
-              <td>03/18/22</td>
-              <td>24</td>
-              <td>7</td>
-              <td>
-                <span class="bi bi-pencil me-3"></span>
-                <span class="bi bi-trash3"></span>
-              </td>
-            </tr>
-            <tr>
-              <td scope="row">Dreams</td>
-              <td>03/18/22</td>
-              <td>24</td>
-              <td>7</td>
-              <td>
-                <span class="bi bi-pencil me-3"></span>
-                <span class="bi bi-trash3"></span>
-              </td>
-            </tr>
-            <tr>
-              <td scope="row">Dreams</td>
-              <td>03/18/22</td>
-              <td>24</td>
-              <td>7</td>
-              <td>
-                <span class="bi bi-pencil me-3"></span>
-                <span class="bi bi-trash3"></span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <TablePosts />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import TablePosts from '../components/TablePosts.vue'
+
 export default {
-  name: 'Dashboard'
+  name: 'Dashboard',
+  components: {
+    TablePosts
+  }
 }
 </script>
 
