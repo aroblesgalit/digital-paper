da9b653879f4c0bc08e5f091942175d4036087fa
Alvin Galit <aroblesgalit@gmail.com>
2022-03-29 11:56:39 -0600

    Created component for table of posts

---
+++ client/src/components/TablePosts.vue
@@ -0,0 +1,73 @@
+<template>
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th scope="col">Title</th>
+        <th scope="col">Date created</th>
+        <th scope="col">Likes</th>
+        <th scope="col">Comments</th>
+        <th class="col">&nbsp;</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td scope="row">Dreams</td>
+        <td>03/18/22</td>
+        <td>24</td>
+        <td>7</td>
+        <td>
+          <span class="bi bi-pencil me-3"></span>
+          <span class="bi bi-trash3"></span>
+        </td>
+      </tr>
+      <tr>
+        <td scope="row">Dreams</td>
+        <td>03/18/22</td>
+        <td>24</td>
+        <td>7</td>
+        <td>
+          <span class="bi bi-pencil me-3"></span>
+          <span class="bi bi-trash3"></span>
+        </td>
+      </tr>
+      <tr>
+        <td scope="row">Dreams</td>
+        <td>03/18/22</td>
+        <td>24</td>
+        <td>7</td>
+        <td>
+          <span class="bi bi-pencil me-3"></span>
+          <span class="bi bi-trash3"></span>
+        </td>
+      </tr>
+      <tr>
+        <td scope="row">Dreams</td>
+        <td>03/18/22</td>
+        <td>24</td>
+        <td>7</td>
+        <td>
+          <span class="bi bi-pencil me-3"></span>
+          <span class="bi bi-trash3"></span>
+        </td>
+      </tr>
+      <tr>
+        <td scope="row">Dreams</td>
+        <td>03/18/22</td>
+        <td>24</td>
+        <td>7</td>
+        <td>
+          <span class="bi bi-pencil me-3"></span>
+          <span class="bi bi-trash3"></span>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'TablePosts'
+}
+</script>
+
+<style></style>
