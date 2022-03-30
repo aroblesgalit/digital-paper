90900330c41831792cba870858ed890c953b23a3
Alvin Galit <aroblesgalit@gmail.com>
2022-03-29 20:49:35 -0600

    Formatted how much time has passed since post date

--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -5,31 +5,25 @@
         <img src="https://via.placeholder.com/60" alt="" />
       </div>
       <div class="d-flex flex-column justify-content-center">
-        <p class="m-0">Persons name</p>
-        <span>1d</span>
+        <p class="m-0">{{ post.author }}</p>
+        <span>{{ formattedTime }}</span>
       </div>
     </div>
     <div class="body card-body">
-      <h5 class="card-title">Post title</h5>
+      <h5 class="card-title">{{ post.title }}</h5>
       <p class="card-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non quam
-        quis nibh elementum lacinia. Curabitur maximus dapibus risus sed porta.
-        Quisque enim elit, cursus a elit a, elementum gravida leo. Donec vitae
-        massa et tellus tempus pretium. Aliquam volutpat porttitor magna in
-        facilisis. Sed vel placerat leo, ac vehicula nulla. Duis odio leo,
-        consequat at viverra nec, iaculis eu odio. Duis facilisis, dolor vel
-        consequat maximus, dolor lacus tempus neque, a malesuada felis mi ut
-        lacus. Integer sed augue ut nulla rutrum pretium quis vel purus. Etiam
-        quam odio, gravida non quam et, venenatis consequat nunc. Sed pretium
-        placerat ullamcorper. Suspendisse vestibulum imperdiet ligula et
-        faucibus. In in blandit arcu, et pellentesque tortor.
+        {{ post.body }}
       </p>
     </div>
     <div class="footer card-body d-flex justify-content-between">
       <a href="#" class="card-link">Read more</a>
       <div class="d-flex">
-        <div class="me-4"><span class="bi bi-heart"> 24</span></div>
-        <div><span class="bi bi-chat"> 7</span></div>
+        <div class="me-4">
+          <span class="bi bi-heart">&nbsp;{{ post.likes }}</span>
+        </div>
+        <div>
+          <span class="bi bi-chat">&nbsp;{{ post.comments.length }}</span>
+        </div>
       </div>
     </div>
   </div>
@@ -37,7 +31,36 @@
 
 <script>
 export default {
-  name: 'Post'
+  name: 'Post',
+  props: {
+    post: Object
+  },
+  computed: {
+    formattedTime () {
+      let datePosted = new Date(this.post.createdAt)
+      let dateNow = new Date()
+
+      if (
+        datePosted.getMonth() === dateNow.getMonth() &&
+        datePosted.getDate() === dateNow.getDate() &&
+        datePosted.getFullYear() === dateNow.getFullYear()
+      ) {
+        let difference = dateNow - datePosted
+        let seconds = Math.floor((difference / 1000) % 60)
+        let minutes = Math.floor((difference / (1000 * 60)) % 60)
+        let hours = Math.floor((difference / (1000 * 60 * 60)) % 24)
+        if (hours < 1 && minutes < 1) {
+          return seconds + 's'
+        } else if (hours < 1) {
+          return minutes + 'm'
+        } else {
+          return hours + 'h'
+        }
+      } else {
+        return 'Posted day(s) ago!'
+      }
+    }
+  }
 }
 </script>
 
@@ -45,6 +68,7 @@ export default {
 .card {
   width: 450px;
   max-width: 90vw;
+  margin-bottom: 16px;
 }
 .card .header img {
   border-radius: 50%;
