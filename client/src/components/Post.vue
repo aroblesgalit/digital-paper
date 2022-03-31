e02ea2f997b78433bd2c871f8640a8a353a657a3
Alvin Galit <aroblesgalit@gmail.com>
2022-03-30 21:16:45 -0600

    Updated the props for author in Post component

--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -5,7 +5,7 @@
         <img src="https://via.placeholder.com/60" alt="" />
       </div>
       <div class="d-flex flex-column justify-content-center">
-        <p class="m-0">{{ post.authorName || post.author }}</p>
+        <p class="m-0">{{ post.author.username }}</p>
         <span>{{ formattedTime }}</span>
       </div>
     </div>
