4f9c9f7fd7fcf4f368a4e78ea433cd9712f45e78
Alvin Galit <aroblesgalit@gmail.com>
2022-04-06 22:14:30 -0600

    Created method for updating user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -35,5 +35,10 @@ module.exports = {
         username: req.user.username
       })
     }
+  },
+  updateUser: function (req, res) {
+    db.User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
   }
 }
