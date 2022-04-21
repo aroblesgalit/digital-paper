73f3b1e8d15b618795a97f54a9f81966aa23fde6
Alvin Galit <aroblesgalit@gmail.com>
2022-04-21 09:58:51 -0600

    Commented out all functions inside the pre deleteOne hook

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const bcrypt = require('bcrypt')
 const saltRounds = 10
+const db = require('./index')
 
 const userSchema = new Schema({
   username: {
@@ -45,22 +46,44 @@ userSchema.pre('save', async function save (next) {
   }
 })
 
-userSchema.pre('deleteOne', { document: true, query: false }, function (next) {
-  // First, delete user's comments from all posts' comments array
-  // Second, delete all comment docs from user's posts
-  // Third, delete all the posts and comments that reference the deleted user
+userSchema.pre('deleteOne', { document: true, query: false }, async function (
+  next
+) {
+  try {
+    // Find all user's comments
+    const allComments = await this.model('Comment').findAll({
+      commenter: this._id
+    })
+    console.log(allComments)
+    // First, delete user's comments from all posts' comments array
+    // allComments.then(comment => {
+    //   await this.model('Post').updateMany(
+    //     { comments: { $in: comment._id} },
+    //     {
+    //       $pullAll: {
+    //         comments: comment._id
+    //       }
+    //     }
+    //   )
+    // })
 
-  this.model('Post').deleteMany({ author: this._id }, next)
-  const commentsByUser = this.model('Comments').findAll({
-    commenter: { $in: [this.id] }
-  })
-  // this.model('Post').updateMany(
-  //   { comments: { $in: [this._id] } },
-  //   { $pullAll: { comments: this._id } },
-  //   next
-  // )
-  console.log(commentsByUser)
-  this.model('Comment').deleteMany({ commenter: this._id }, next)
+    // Second, delete all comment docs from user's posts
+    // Third, delete all the posts and comments that reference the deleted user
+
+    // this.model('Post').deleteMany({ author: this._id }, next)
+    // const commentsByUser = this.model('Comments').findAll({
+    //   commenter: { $in: [this._id] }
+    // })
+    // this.model('Post').updateMany(
+    //   { comments: { $in: [this._id] } },
+    //   { $pullAll: { comments: this._id } },
+    //   next
+    // )
+    // console.log(commentsByUser)
+    // this.model('Comment').deleteMany({ commenter: this._id }, next)
+  } catch (err) {
+    next(err)
+  }
 })
 
 userSchema.methods.validatePassword = async function validatePassword (data) {
