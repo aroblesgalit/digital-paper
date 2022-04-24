bc89ef89fe4e2426da6e9820c9029099f6015529
Alvin Galit <aroblesgalit@gmail.com>
2022-04-23 19:38:06 -0600

    Took out comment._id from an array

--- models/user.js
+++ models/user.js
@@ -51,52 +51,21 @@ userSchema.pre('deleteOne', { document: true, query: false }, async function (
   next
 ) {
   try {
-    // Find all user's comments
-    // db.Comment.find({ commenter: this._id })
-    //   .then(res => res.json())
-    //   .then(data => {
-    //     console.log(data)
-    //     data.map(comment => {
-    //       db.Post.updateMany(
-    //         {
-    //           comments: { $in: comment._id }
-    //         },
-    //         {
-    //           $pullAll: {
-    //             comments: comment._id
-    //           }
-    //         }
-    //       )
-    //     })
-    //   })
-    //   .catch(err => console.error(err))
     // First, delete user's comments from all posts' comments array
     const userComments = await Comment.find({ author: this._id })
     userComments.forEach(comment => {
       Post.updateMany(
         { comments: { $in: comment._id } },
-        { $pull: { comments: [comment._id] } },
+        { $pull: { comments: comment._id } },
         function (err, res) {
           if (err) return next(err)
           next()
         }
       )
     })
-    // allComments.then(comment => {
-    //   await this.model('Post').updateMany(
-    //     { comments: { $in: comment._id} },
-    //     {
-    //       $pullAll: {
-    //         comments: comment._id
-    //       }
-    //     }
-    //   )
-    // })
-
     // Second, delete all comment docs from user's posts
     const userPosts = await Post.find({ author: this._id })
     userPosts.forEach(post => {
-      console.log(post)
       Comment.deleteMany({ _id: { $in: post.comments } }, function (
         err,
         result
@@ -105,26 +74,12 @@ userSchema.pre('deleteOne', { document: true, query: false }, async function (
         next()
       })
     })
-
     // Third, delete all the posts and comments that reference the deleted user
-
-    // Deletes user's post docs but not the posts' comments arrays
     this.model('Post').deleteMany({ author: this._id }, function (err, result) {
       if (err) return next(err)
       // db.Comment.deleteMany({ _id: { $in: result.comments } })
       next()
     })
-    // const commentsByUser = this.model('Comments').findAll({
-    //   commenter: { $in: [this._id] }
-    // })
-    // this.model('Post').updateMany(
-    //   { comments: { $in: [this._id] } },
-    //   { $pullAll: { comments: this._id } },
-    //   next
-    // )
-    // console.log(commentsByUser)
-
-    // Delete user's comment docs but not from post arrays
     this.model('Comment').deleteMany({ commenter: this._id }, next)
   } catch (err) {
     next(err)
