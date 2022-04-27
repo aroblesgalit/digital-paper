0feb7818ff0446d5148924b231d60e7c263a972c
Alvin Galit <aroblesgalit@gmail.com>
2022-04-27 13:35:12 -0600

    Created updateComment function in controller

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -7,6 +7,15 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err))
   },
+  updateComment: function (req, res) {
+    db.Comment.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    )
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
+  },
   deleteComment: async function (req, res) {
     try {
       const commentToDelete = await db.Comment.findOne({ _id: req.params.id })
