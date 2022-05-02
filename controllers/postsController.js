df6f08a4590e32d0c5dd3a3153bc224a02e43e94
Alvin Galit <aroblesgalit@gmail.com>
2022-05-01 22:33:42 -0600

    Created function for adding user id to likes array

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -76,5 +76,18 @@ module.exports = {
       ])
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err))
+  },
+  likePost: function (req, res) {
+    db.Post.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: {
+          likes: req.body._id
+        }
+      },
+      { new: true }
+    )
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
   }
 }
