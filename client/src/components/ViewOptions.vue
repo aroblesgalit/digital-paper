ee989997a2c8bab66abb121dabf1aa02e3bcca06
Alvin Galit <aroblesgalit@gmail.com>
2022-05-03 19:20:17 -0600

    Created component for view options with filter and sort selects

---
+++ client/src/components/ViewOptions.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mb-1">
+    <div>
+      <span>Filter:</span>
+      <select class="form-select form-select-sm" aria-label="form select sm">
+        <option value="all">All</option>
+        <option value="poetry">Poetry</option>
+        <option value="story">Story</option>
+        <option value="journal">Journal</option>
+        <option value="lyrics">Lyrics</option>
+      </select>
+    </div>
+    <div>
+      <span>Sort:</span>
+      <select class="form-select form-select-sm" aria-label="form select sm">
+        <option value="top">Top</option>
+        <option value="new">New</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ViewOptions'
+}
+</script>
+
+<style scoped></style>
